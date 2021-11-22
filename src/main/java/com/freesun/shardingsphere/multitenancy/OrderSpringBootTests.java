4d9b581e31bb9b32d6589368b90146c3077d1998
fyname <fyname@163.com>
2021-11-22 09:29:49 +0800

    Update OrderSpringBootTests.java
    del sharding

--- src/main/java/com/freesun/shardingsphere/multitenancy/OrderSpringBootTests.java
+++ src/main/java/com/freesun/shardingsphere/multitenancy/OrderSpringBootTests.java
@@ -7,7 +7,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import javax.annotation.Resource;
@@ -23,7 +22,6 @@ import java.util.Random;
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(classes = OrderSpringBootTests.class)
 @SpringBootApplication
-@ActiveProfiles("sharding")
 public class OrderSpringBootTests {
 
     @Resource
